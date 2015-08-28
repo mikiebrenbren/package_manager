3ec0a472b57b2f9e21df6fcd9a3cc349d0db450e
Michael Brennan <Michael Brennan>
2015-08-28 08:43:28 -0600

    first test working with only one package with a dependency, failing when attempting to scale

--- src/main/java/com/mike/PackageManager.java
+++ src/main/java/com/mike/PackageManager.java
@@ -1,13 +1,39 @@
 package com.mike;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by michaelbrennan on 8/27/15.
  */
-public class PackageManager {
+public class PackageManager{
 
+    //separate everything with a dependency with things that do not have a dependency
+    //into different data structures.
     public String[] orderPacks(String[] packages){
 
-        return null;
-    }
+        //pattern to find what is after the colon
+        Pattern p = Pattern.compile(".*:\\s*(.*)");
+        Matcher m;
+        //array to hold a package with a dependency
+        String[] split;
+        List<String> output = new ArrayList<String>();
+        for(String s : packages){
+            m = p.matcher(s);
+            if(m.find() && !m.group(1).equals("")){
+                split = s.split("\\:");
+                output.add(0, split[0].replaceAll("\\s", ""));
+                output.add(output.size()-1, split[split.length-1].replaceAll("\\s", ""));
+                for (String sp : output){
+                    System.out.println(sp);
+                }
+            }
+        }
 
+        return output.toArray(new String[output.size()]);
+    }
 }
--- src/test/groovy/TestMe.groovy
+++ src/test/groovy/TestMe.groovy
@@ -2,8 +2,12 @@ package groovy
 
 import com.mike.Hello
 import com.mike.PackageManager
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Stepwise
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 /**
  * Created by michaelbrennan on 8/27/15.
@@ -11,27 +15,44 @@ import spock.lang.Stepwise
 @Stepwise
 class TestMe extends Specification {
 
+    @Shared
+    PackageManager pm
+
     def "test me "(){
         when:
         Hello h = new Hello(5)
 
         then:
         h.getS() == 5
-//        h.getS() !=5
     }
 
     def "Package manager 1"(){
         when:
-        PackageManager pm = new PackageManager();
+        pm = new PackageManager()
         String[] input = ["KittenService: CamelCaser", "CamelCaser:"]
         String[] iwant = ["CamelCaser", "KittenService"]
         String [] output = pm.orderPacks(input)
 
         then:
         iwant as Set == output as Set
-
+        for(int i = 0; i < iwant.length; i++){
+            assert iwant[i].equals(output[i])
+        }
     }
 
+    def "Package manager 2"(){
+        when:
+        pm = new PackageManager()
+        String[] input = ["KittenService:", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme",
+        "Ice:"]
+        String[] iwant = ["KittenService", "Ice", "Cyberportal", "Leetmeme", "CamelCaser", "Fraudstream"]
+        String [] output = pm.orderPacks(input)
 
+        then:
+        iwant as Set == output as Set
+        for(int i = 0; i < iwant.length; i++){
+            assert iwant[i].equals(output[i])
+        }
+    }
 
 }
